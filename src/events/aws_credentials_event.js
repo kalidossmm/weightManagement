f71630f1dc295eaf56ae47854fbb1fc34284ddb4
Samishthi <sami@Samishthis-MacBook-Pro.local>
2017-02-16 00:46:45 -0800

    unwanted files removed

--- src/events/aws_credentials_event.js
+++
@@ -1,9 +0,0 @@
-var EventEmitter = require('events');
-var util = require('util');
-
-function CredentialsLoadedEvent() {
-    EventEmitter.call(this);
-}
-util.inherits(CredentialsLoadedEvent, EventEmitter);
-
-module.exports = new CredentialsLoadedEvent();
